ccb3b1441fbce73c96320a39df23591dcf0df9f1
reymundadwi <reymunda613@gmail.com>
2021-11-15 20:54:13 +0700

    routing system

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const http = require('http');
 
 const server =http.createServer((req,res) => {
-    res.write('Hello');
-    res.end();
+    let url = req.url;
+    
+    res.writeHead(200,{
+        'Content-Type' : 'text/html'
+    })
+
+    if(url === '/about'){
+        res.write('About');
+        res.end();
+    }else if(url === '/contact'){
+        res.write('Contact')
+        res.end()
+    }else{
+        res.write('Hello world');
+        res.end();
+    }
 })
 
 server.listen(3000,() => {
