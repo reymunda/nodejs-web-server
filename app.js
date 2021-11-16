9458434f215ac113cd52969ca61a1edb7d4bf409
reymundadwi <reymunda613@gmail.com>
2021-11-16 17:57:49 +0700

    EJS implementation in index

--- app.js
+++ app.js
@@ -4,14 +4,38 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine','ejs');
+
 app.get('/',(req,res) => {
-    res.sendFile('./index.html',{root: __dirname});
+    // res.sendFile('./index.html',{root: __dirname});
+    let hobby = [
+        {
+            name: 'Coding',
+            tool: 'Laptop'
+        },
+        {
+            name: 'Football',
+            tool: 'Ball'
+        },
+        {
+            name: 'Sing',
+            tool: 'Mic'
+        }
+    ]
+
+    res.render('index',{
+        name: 'Reymunda',
+        hobby        
+    });
+
 })
 app.get('/about',(req,res) => {
-    res.sendFile('./about.html',{root: __dirname});
+    // res.sendFile('./about.html',{root: __dirname});
+    res.render('about');
 })
 app.get('/contact',(req,res) => {
-    res.sendFile('./contact.html',{root: __dirname});
+    // res.sendFile('./contact.html',{root: __dirname});
+    res.render('contact');
 })
 app.get('/category/:id/product/:code',(req,res) => {
     res.send(`Category id: ${req.params.id} \n Product code: ${req.params.code} \n Search for: ${req.query.search}`);
